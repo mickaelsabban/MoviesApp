fb7efd322eeb861cc39c9744b67d0a64211366a8
mickaelsabban <mickael.sabban@gmail.com>
2015-08-29 21:47:41 -0400

    chage name var

---
+++ MovieApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieApp
+//
+//  Created by lea on 8/28/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindodasdaw *window;
+
+@end
